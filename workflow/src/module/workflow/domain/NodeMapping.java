49a48c1d7fe60c00f262810dd6ba376d064928f6
Paulo Abrantes <paulo.abrantes@ist.utl.pt>
2010-08-31 14:35:52 +0000

    Bug fix: Remove workflow module dependency from myorg.  Refactor the code into a dependency injection through a relation listener.

--- workflow/src/module/workflow/domain/NodeMapping.java
+++ workflow/src/module/workflow/domain/NodeMapping.java
@@ -1,9 +1,31 @@
 package module.workflow.domain;
 
+import dml.runtime.RelationAdapter;
+import module.organization.domain.Party;
+import module.organization.domain.PartyType;
+import module.organizationIst.domain.IstPartyType;
+import myorg.domain.MyOrg;
 import myorg.domain.contents.Node;
+import pt.ist.expenditureTrackingSystem.domain.organization.CostCenter;
 import pt.ist.fenixWebFramework.services.Service;
 
 public class NodeMapping extends NodeMapping_Base implements Comparable<NodeMapping> {
+
+    public static class NodeListenner extends RelationAdapter<Node, MyOrg> {
+
+	@Override
+	public void afterRemove(final Node node, final MyOrg myorg) {
+	    if (node != null && myorg != null) {
+		for (; !node.getNodeMapping().isEmpty(); node.getNodeMapping().get(0).delete())
+		    ;
+	    }
+	}
+
+    }
+
+    static {
+	Node.MyOrgNode.addListener(new NodeListenner());
+    }
 
     protected NodeMapping(Node node, int order) {
 	super();
