9e4d3fc1a3bf6ed23e11d17dd895628ad91b72a8
João Neves <joaoroxoneves@ist.utl.pt>
2011-09-30 15:52:37 +0000

    [Meta Workflow] Minor Changes: Resolved presentation issues with the description of the ChangeMetaQueue activity in the logs.

--- meta-workflow/src/module/metaWorkflow/activities/ChangeMetaQueue.java
+++ meta-workflow/src/module/metaWorkflow/activities/ChangeMetaQueue.java
@@ -1,7 +1,12 @@
 package module.metaWorkflow.activities;
 
+import java.util.Iterator;
+
 import module.metaWorkflow.domain.WorkflowMetaProcess;
 import module.workflow.activities.ChangeQueue;
+import module.workflow.activities.ChangeQueueInformation;
+import module.workflow.domain.WorkflowQueue;
+import myorg.util.BundleUtil;
 
 public class ChangeMetaQueue extends ChangeQueue<WorkflowMetaProcess> {
 
@@ -13,5 +18,45 @@ public class ChangeMetaQueue extends ChangeQueue<WorkflowMetaProcess> {
     @Override
     public boolean isDefaultInputInterfaceUsed() {
 	return false;
+    }
+
+    @Override
+    protected String[] getArgumentsDescription(ChangeQueueInformation<WorkflowMetaProcess> activityInformation) {
+	String queuesDescription = "";
+	if (activityInformation.hasAnyQueuesToRemove()) {
+	    if (activityInformation.hasAnyQueuesToAdd()) {
+		queuesDescription += BundleUtil.getStringFromResourceBundle(getUsedBundle(), "label.from");
+	    } else {
+		queuesDescription += BundleUtil.getStringFromResourceBundle(getUsedBundle(), "label.removedFrom");
+	    }
+
+	    queuesDescription += " ";
+	    Iterator<WorkflowQueue> queuesToRemove = activityInformation.getQueuesToRemove().iterator();
+	    if (queuesToRemove.hasNext()) {
+		queuesDescription += queuesToRemove.next().getName();
+	    }
+	    while (queuesToRemove.hasNext()) {
+		queuesDescription += ", " + queuesToRemove.next().getName();
+	    }
+	}
+
+	if (activityInformation.hasAnyQueuesToAdd()) {
+	    if (activityInformation.hasAnyQueuesToRemove()) {
+		queuesDescription += " | ";
+	    }
+	    queuesDescription += BundleUtil.getStringFromResourceBundle(getUsedBundle(), "label.to");
+
+	    queuesDescription += " ";
+	    Iterator<WorkflowQueue> queuesToAdd = activityInformation.getQueuesToAdd().iterator();
+	    if (queuesToAdd.hasNext()) {
+		queuesDescription += queuesToAdd.next().getName();
+	    }
+	    while (queuesToAdd.hasNext()) {
+		queuesDescription += ", " + queuesToAdd.next().getName();
+	    }
+	}
+
+	String[] arguments = { queuesDescription };
+	return arguments;
     }
 }
